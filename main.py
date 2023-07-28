7a36ca54cfd17f6a4707d171d97cfa5855b7006e
d-derug <3nehene@gmail.com>
2023-07-28 16:25:05 +0300

    Flask-shop(2)

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from cloudipsp import Api, Checkout
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    isActivate = db.Column(db.Boolean, default=True)
+
+    def __repr__(self):
+        return self.title
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = Item.query.order_by(Item.price).all()
+    return render_template('index.html', data=items)
 
 
 @app.route('/about')
@@ -13,6 +29,40 @@ def about():
     return render_template('about.html')
 
 
-    return render_template('')
+@app.route('/buy/<int:id>')
+def buy(id):
+    item=Item.query.get(id)
+
+    api = Api(merchant_id=1396424,
+          secret_key='test')
+    checkout = Checkout(api=api)
+    data = {
+    "currency": "RUB",
+    "amount": str(item.price) + "00"
+    }
+    url = checkout.url(data).get('checkout_url')
+    return redirect(url)
+
+
+@app.route('/create-product', methods=['POST', 'GET'])
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        price = request.form['price']
+        text = request.form['text']
+
+        item = Item(title=title, price=price, text=text)
+
+        try:
+             db.session.add(item)
+             db.session.commit()
+             return redirect('/')
+        except:
+            return "Получилась ошибка"
+    else:
+        return render_template('create_product.html')
+          
+
+
 if __name__ == "__main__":
-   app.run(debug=True)
+    app.run(debug=True)
---
+++ venv/Lib/site-packages/cloudipsp/configuration.py
@@ -0,0 +1,4 @@
+__api_url__ = 'https://{api_domain}/api'
+__sign_sep__ = '|'
+__protocol__ = '1.0'
+__r_type__ = 'json'
